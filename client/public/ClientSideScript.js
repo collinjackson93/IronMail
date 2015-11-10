58a4bbd53dfb0c8fb5d38c0e9f87e4ab4c2c7126
Grayson Brewer <e.grayson.brewer@gmail.com>
2015-11-10 01:05:23 -0600

    added fncts for login & abstraction implemented

--- client/public/ClientSideScript.js
+++ client/public/ClientSideScript.js
@@ -4,20 +4,22 @@
 
 /* AJAX request function that requires response
    CITATION: GOT THIS FROM CODE I WROTE PREVIOUSLY FOR the ORACLEOFOMAHA assignment */
-function getPageWithCallback(url, callback) {
+function getPageWithCallback(url, signingUpFlag, callback) {
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
+
         if (httpRequest.readyState == 4 && httpRequest.status == 200)
             callback(httpRequest.responseText);
-    }
+    };
 
     httpRequest.open("GET", url, true);
     httpRequest.send(null);
 }
 
+
 function signUpForIronMail(name, id, passwd){
 
-    getPageWithCallback('/addNewUser?name=' + name + '&id=' + id + '&passwd=' + passwd, function(response){
+    getPageWithCallback('/addNewUser?name=' + name + '&id=' + id + '&passwd=' + passwd, true, function(response){
         if(!response){
             alert('that username is not available, try another');
         }
@@ -27,3 +29,25 @@ function signUpForIronMail(name, id, passwd){
 
     });
 }
+
+
+function logInIronMail(name, passwd) {
+
+    // need to replace url param with proper input
+    getPageWithCallback(url, false, function(response){
+        if(!response) {
+            var passwordIsCorrect = verifyPassword(passwd, function(verification){
+                //TODO: logic for verifying password with server goes here
+            });
+            if (passwordIsCorrect) {
+                console.log('success');
+            }
+
+        }
+        else{
+            alert('username not found')
+        }
+    });
+
+}
+
