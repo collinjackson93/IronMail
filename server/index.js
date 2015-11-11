ee6c4b9527ecad52d6f1a218be30585fdd7d06f2
collinjackson93 <collinjackson93@gmail.com>
2015-11-10 21:03:26 -0600

    Took out Monk in an effort to fix kerberos issue. Took out libkrb5-dev

--- server/index.js
+++ server/index.js
@@ -1,45 +1,8 @@
 var express = require('express');
 var app = express();
-var db = require('monk')('mongo:27017/test');
-var users = db.get('users');
-var path = require('path');
-
-users.insert({name: 'World'});
 
 app.get('/', function (req, res) {
-  try {
-    users.findOne({}, function(err, doc) {
-      res.send('Hello ' + doc.name + '!');
-    });
-  } catch (e) {
-    res.send("Something went wrong!");
-  }
-});
-
-app.get('/len', function(req, res) {
-  users.find({}, function(err, docs) {
-    try {
-      res.send('' + docs.length);
-    }
-    catch(exception){
-      res.send(exception);
-    }
-  });
-});
-
-app.get('/insert', function(req, res){
-  users.insert({name: 'World'});
-  res.send("inserted");
-});
-
-app.get('/clear', function(req, res) {
-  users.remove({}, function(err) {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send('Successfully cleared database');
-    }
-  });
+  res.send('Testing')
 });
 
 var server = app.listen(3000, function () {
