92f76b4ae95e38cc5240a9f7b563630ba69c950b
Grayson Brewer <e.grayson.brewer@gmail.com>
2015-11-15 22:07:13 -0600

    added skeletons for functions, debug msgs

--- client/server.js
+++ client/server.js
@@ -2,14 +2,54 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var cors = require('cors');
+//var router = require('IronMail/server/node/router.js');
+//var forever = require('IronMail/server/node/forever.js');
+
+var loginPage = "IronMail.html";
 
 app.use(express.static(path.join("./", 'public')));
 app.use(cors());
 
-app.get('/', function (req, res) {
-  res.sendFile("IronMail.html", {root: __dirname});
-});
-
 var server = app.listen(5000, function () {
   console.log("Server listening at http://localhost:5000");
+  /*var pathname = server.pathname;
+  var port = server.port;*/
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(loginPage, {root: __dirname});
+    console.log("sent the page");
+});
+
+var onSignUpClicked = function() {
+    console.log("this guy's signing up");
+    document.write("sign me up scottie");
+};
+
+var onLoginAttempt = function(username, password) {
+    console.log("I hit the goddamn button");
+    res.send("Hello Login Attempt");
+
+    if (validateCredentials(username, password)) {
+        res.send(); //inbox
+    } else {
+        res.send(loginPage, {root: __dirname});
+    }
+    return 'penis';
+
+};
+
+var validateCredentials = function() {
+
+};
+
+var onSignUp = function() {
+    onSignUpClicked();
+    // store user name and password (and public key?)
+}
+
+
+app.post('/', function(req, res) {
+    console.log(req.body);
+    onLoginAttempt(username, password);
 });
