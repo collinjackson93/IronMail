21c106762eef5f62ab14d2575c49e334673b2322
Lawrence Waller <lawrence.a.waller@vanderbilt.edu>
2015-11-17 11:31:15 -0600

    corrected some mistakes

--- client/public/ClientSideScript.js
+++ client/public/ClientSideScript.js
@@ -1,6 +1,9 @@
 /**
  * Created by Lawrence on 11/6/15.
  */
+var areWeSignedUp = false;
+var areWeLoggedIn = false;
+var whoIsLoggedInID = "";
 
 /* AJAX request function that requires response
    CITATION: GOT THIS FROM CODE I WROTE PREVIOUSLY FOR the ORACLEOFOMAHA assignment */
@@ -15,15 +18,44 @@ function getPageWithCallback(url, callback) {
     httpRequest.send(null);
 }
 
+//invokes sign up logic on server
 function signUpForIronMail(name, id, passwd){
-
     getPageWithCallback('/addNewUser?name=' + name + '&id=' + id + '&passwd=' + passwd, function(response){
-        if(!response){
+        if(response = "invalid"){
             alert('that username is not available, try another');
         }
         else{
-            console.log('success');
+            console.log('successful sign up for user ' + response);
         }
 
     });
 }
+
+//invokes log in on server
+function logInToIronMail(id, passwd){
+    getPageWithCallback('/logIn?id=' + id + '&passwd=' + passwd, function(response){
+
+        if(!response){
+            alert('login was invalid');
+        }
+        else{
+            console.log('successful login');
+        }
+
+    })
+}
+
+//invokes log out on server, then wipes html of sensitive data
+function logOutOfIronMail(){
+    getPageWithCallback('/logOut', function(response){
+        whoIsLoggedInID = "";
+        areWeLoggedIn = false;
+        logOut();
+    });
+}
+
+//wipes webpage
+function logOut(){
+    console.log('clear web page');
+}
+
